08965c1e38caae7e65d9b1e89a92afd60584a85c
CarlosHr93 <bet0_93@hotmail.com>
2024-01-17 12:52:37 -0600

    Variables

---
+++ app.js
@@ -0,0 +1,9 @@
+let NumeroUsuario = 0;
+let Numeroganador = 6 ;
+let mensajeDeBienvenida = 'Bienvenido a la aplicación';
+alert('mensajeDeBienvenida');
+
+NumeroUsuario = prompt('Introdusca un numero');
+    if(NumeroUsuario == Numeroganador){
+        alert('ERES GANADOR');
+    }else{alert('SIGUE PARTICIPANDO');}
