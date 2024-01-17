3443e7af5e6f0ee4d3269c350e6d025bdc47e0b4
CarlosHr93 <bet0_93@hotmail.com>
2024-01-17 15:49:00 -0600

    SECCION 1 JS

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
-let NumeroUsuario = 0;
-let Numeroganador = 6 ;
-let mensajeDeBienvenida = 'Bienvenido a la aplicación';
-alert('mensajeDeBienvenida');
+// let NumeroUsuario = 0;
+// let Numeroganador = 6 ;
 
-NumeroUsuario = prompt('Introdusca un numero');
-    if(NumeroUsuario == Numeroganador){
-        alert('ERES GANADOR');
-    }else{alert('SIGUE PARTICIPANDO');}
+// let mensajeDeBienvenida = 'Bienvenido a la aplicación';
+// alert('mensajeDeBienvenida');
+
+// NumeroUsuario = prompt('Introdusca un numero');
+//     if(NumeroUsuario == Numeroganador){
+//         alert('ERES GANADOR');
+//     }else{alert('SIGUE PARTICIPANDO');}
+
+//     DESAFIO 1
+// alert('¡Bienvenida y bievenido a nuestro sitio web!');
+
+//     DESAFIO 2
+// let Nombre = "luna" ;
+
+//     DESAFIO 3
+// let Edad = 25 ;
+
+//let anumeroDeVentas = 50;
+
+//let numeroDeVentas = 1000;
+
+//alert('¡Error! Completa todos los campos');
+
+// let mensajeError = '¡Error! Completa todos los campos';
+// // alert(mensajeError);
+
+// let NombreUsuario = prompt('Introdusca su nombre:');
+
+// let Edad = prompt('Introdusca su edad:');
+//     if(Edad <= 18){
+//         alert("¡Puedes obtener tu licencia de conducir! ,"+NombreUsuario);
+//     }else{alert('¡NO Puedes obtener tu licencia de conducir!');}
