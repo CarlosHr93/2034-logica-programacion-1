5cf57ed5254f16634d2e7ea70fc867609ff40d84
CarlosHr93 <bet0_93@hotmail.com>
2024-01-20 16:17:29 -0600

    completado bloque 3 part1 de alura

--- video.js
+++ video.js
@@ -1,9 +1,23 @@
 let NumeroSecreto = 6;
 let NumeroUsuario = 0;
-
-NumeroUsuario = prompt('instrodusca un numero de entre 1 y 10');
-if(NumeroUsuario == NumeroSecreto){
-    alert(Acertaste)
-}else{
-    alert('Nose acertaste sigue intentando')
+let intentos = 1;
+let palabra = 'ves';
+    
+while(NumeroUsuario != NumeroSecreto){
+    NumeroUsuario = prompt('instrodusca un numero de entre 1 y 10');
+    if(NumeroUsuario==NumeroSecreto){
+        alert(`Acertaste , el numero secreto es ${NumeroSecreto} . Lo hiciste en ${intentos} ${palabra} .`);
+    }else{
+        intentos = intentos+1;
+        palabra = `veces`;
+    if(NumeroUsuario<NumeroSecreto){
+        alert(`muy cerca el numero secreto es Mayor`);
+    }else {
+        alert(`muy cerca el numero secreto es menor`);
+    }
+    if(intentos == 3){
+        alert(`FIN DE JUEGOS SUPERERASTE LOS INTENTOS`);
+        break;
+    }    
+}
 }
